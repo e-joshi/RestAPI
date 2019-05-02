034e00e20d6f45744b4e693053d275a1655a1c80
Your Name <europejoshi@gmail.com>
2019-05-02 14:53:24 -0400

    first commit

---
+++ src/test/java/apiService/DemoTest.java
@@ -0,0 +1,41 @@
+package apiService;
+
+import static org.apache.http.HttpStatus.*;
+//import org.apache.http.HttpStatus; // this is for the priorly used.
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+public class DemoTest {
+
+    @Test
+    public void makeSureItWorks(){
+        final String urlTest = "http://services.groupkt.com/state/get/USA/all";
+
+        RestTemplate restTemplate = new RestTemplate();
+        String result = restTemplate.getForObject(urlTest,String.class, SC_OK);
+        System.out.println(result);
+    }
+
+    final String url = "http://dummy.restapiexample.com/api/v1/employees";
+
+    @Test
+    public void empApiTestCall(){
+        RestTemplate restTemplate = new RestTemplate();
+        String result = restTemplate.getForObject(url, String.class);
+        System.out.println(result);
+    }
+
+    @Test
+    public void empStatusCode(){
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<String> responseEntity = restTemplate.getForEntity(url, String.class);
+        String code = String.valueOf(responseEntity.getStatusCode());
+
+        Assert.assertEquals("200 OK", code);
+
+    }
+
+}
